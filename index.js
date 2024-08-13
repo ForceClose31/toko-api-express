3e54b2b60f3d7b495a1ac16198acc82671811505
ForceClose31 <forceclose31@gmail.com>
2024-08-14 01:22:07 +0700

    git ignore commit

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const express = requre('express');
+const express = require('express');
 const app = express();
+
+// basic route
+// app.method(path, handler);
+app.use('/', (req, res, next) => {
+    res.send('Hello World')
+})
+
 
 app.listen(4000, () => {
     console.log('Server success running');
